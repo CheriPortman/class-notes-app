4bc3f39de996d4f138c51ef40c59b0e595f35b9a
Cheri Portman <cheriportman@hotmail.com>
2019-06-10 10:46:35 -0700

    finished notesReducer and test = newNote import is not currently being used

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,4 +1,4 @@
-
+import { NEW_NOTE_PENDING, NEW_NOTE } from '../actions/notesActions';
 
 const initialState = {
   loading: false,
@@ -6,5 +6,12 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
-  
+  switch(action.type) {
+    case NEW_NOTE_PENDING:
+      return { ...state, loading: true };
+    case NEW_NOTE:
+      return { ...state, loading: false, list: [...state.list, action.payload] };
+    default: 
+      return state;
+  }
 }
---
+++ src/reducers/notesReducer.test.js
@@ -0,0 +1,39 @@
+import reducer from './notesReducer';
+import { newNote, NEW_NOTE_PENDING, NEW_NOTE } from '../actions/notesActions';
+
+describe('notes reducer', () => {
+  it('handles the NEW_NOTE_PENDING action', () => {
+    const initialState = { 
+      loading: false,
+      list: []
+    };
+    const newState = reducer(initialState, {
+      type: NEW_NOTE_PENDING
+    });
+  
+    expect(newState).toEqual({
+      loading: true,
+      list: []
+    });
+  });
+
+  it('handles the NEW_NOTE action', () => {
+    const initialState = {
+      loading: true,
+      list: []
+    };
+
+    const newState = reducer(initialState, {
+      type: NEW_NOTE,
+      payload: {
+        title: 'heeeelo',
+        body: 'nap'
+      }
+    });
+    
+    expect(newState).toEqual({
+      loading: false,
+      list: [{ title: 'heeeelo', body: 'nap' }]
+    });
+  });
+});
