c00088a7845fcc2a8d2883072a394b02d05c1124
Cheri Portman <cheriportman@hotmail.com>
2019-06-11 08:25:26 -0700

    notesReducer and test (passing)

--- src/reducers/notesReducer.test.js
+++ src/reducers/notesReducer.test.js
@@ -1,5 +1,10 @@
 import reducer from './notesReducer';
-import { newNote, NEW_NOTE_PENDING, NEW_NOTE } from '../actions/notesActions';
+import { newNote, 
+  NEW_NOTE_PENDING, 
+  NEW_NOTE,
+  FETCH_NOTES_PENDING,
+  FETCH_NOTES  
+} from '../actions/notesActions';
 
 describe('notes reducer', () => {
   it('handles the NEW_NOTE_PENDING action', () => {
@@ -34,6 +39,37 @@ describe('notes reducer', () => {
     expect(newState).toEqual({
       loading: false,
       list: [{ title: 'heeeelo', body: 'nap' }]
+    });
+  });
+
+  it('handles the FETCH_NOTES_PENDING action', () => {
+    const initialState = {
+      loading: false,
+      list: []
+    };
+    const newState = reducer(initialState, {
+      type: FETCH_NOTES_PENDING
+    });
+
+    expect(newState).toEqual({
+      loading: true,
+      list: []
+    });
+  });
+
+  it('handles the FETCH_NOTES action', () => {
+    const initialState = {
+      loading: true,
+      list: []
+    };
+    const newState = reducer(initialState, {
+      type: FETCH_NOTES,
+      payload: [{ title: 'hey', body: 'there' }]
+    });
+
+    expect(newState).toEqual({
+      loading: false,
+      list: [{ title: 'hey', body: 'there' }]
     });
   });
 });
