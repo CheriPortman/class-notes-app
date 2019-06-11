d66212c12d63116abcd71280cf9e5f73df9acd85
Cheri Portman <cheriportman@hotmail.com>
2019-06-11 14:06:24 -0700

    added session to combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import notes from './notesReducer';
+import session from './sessionReducer';
 
 export default combineReducers({ 
-  notes 
+  notes,
+  session
 });
