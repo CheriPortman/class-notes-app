be931a866a23e59fb7d3f5d7b7839ac6cc2415b9
Cheri Portman <cheriportman@hotmail.com>
2019-06-11 16:37:48 -0700

    working on getting env file and netlify vs localhost straight

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,9 @@
 import { WebAuth } from 'auth0-js';
 
 const auth0 = new WebAuth({
-  domain: 'dev-hgz2e141.auth0.com',
-  clientID: 'RvRTqFDNDiFHa0qh1ioFsgAzuEuvYHt1',
-  redirectUri: 'https://condescending-nobel-f71b1a.netlify.com/callback',
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  redirectUri: process.env.AUTH0_CALLBACK,
   responseType: 'token id_token',
   scope: 'openid profile'
 });
